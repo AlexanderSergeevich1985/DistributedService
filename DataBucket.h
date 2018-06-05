b6cedba7f16d981184416279c47a8582ba921cd1
Aleksandr <36114404+AlexanderSergeevich1985@users.noreply.github.com>
2018-06-05 22:49:43 +0300

    Update DataBucket.h

--- DataBucket.h
+++ DataBucket.h
@@ -1,5 +1,23 @@
-#ifndef FASTSTORAGE
-#define FASTSTORAGE
+/**The MIT License (MIT)
+Copyright (c) 2018 by AleksanderSergeevich
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#ifndef DATABUCKET
+#define DATABUCKET
 #pragma once
 #include <QtCore>
 
@@ -106,5 +124,5 @@ private:
     unsigned int filled;
 };
 
-#endif // FASTSTORAGE
+#endif // DATABUCKET
 
